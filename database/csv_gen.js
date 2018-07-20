fe251133237d0108fd85c7e8374c1f6ea994b373
Brian So <soksbrian@gmail.com>
2018-07-20 11:09:59 -0700

    Setup and refactor to style guide

--- database/csv_gen.js
+++ database/csv_gen.js
@@ -1,60 +1,58 @@
 const fs = require('fs');
 
-//helper function
+// helper function
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
 }
 
-//caption generator
+// caption generator
 const adjectives = ['', 'Spacious ~~', 'Clean ~', 'Tidy ~', 'Brand new !', 'The perfect getaway =)', 'Bright', 'Sunny', 'Recently renovated !', 'New fixtures !', 'Original', 'Just like home =)', 'Comfort at its best', 'Pet-friendly ~', 'Air-conditioned'];
 function randomCaption() {
   return adjectives[getRandomIntInclusive(0, adjectives.length - 1)];
 }
 
-//initialize primary key
-let uniq_id = 1;
+// initialize primary key
+let uniqId = 1;
 let str = '';
 
-for (let home_id = 1; home_id <= 100; home_id++) {
-  //handle exterior
+for (let homeId = 1; homeId <= 100; homeId += 1) {
+  // handle exterior
   let order = 1;
-  str += `${uniq_id}, https://s3-us-west-1.amazonaws.com/homesnap/exterior/h${home_id}.jpg, ${home_id}, ${order}, ''\n`;
+  str += `${uniqId}, https://s3-us-west-1.amazonaws.com/homesnap/exterior/h${homeId}.jpg, ${homeId}, ${order}, ''\n`;
   order += 1;
-  uniq_id += 1;
+  uniqId += 1;
 
-  //handle bedroom
-  let numBR = getRandomIntInclusive(1,2);
-  for (let i = 0; i < numBR; i++) {
-    str += `${uniq_id}, https://s3-us-west-1.amazonaws.com/homesnap/bedroom/b${getRandomIntInclusive(1,10)}.jpg, ${home_id}, ${order}, ${randomCaption()}\n`;
+  // handle bedroom
+  const numBR = getRandomIntInclusive(1,2);
+  for (let i = 0; i < numBR; i += 1) {
+    str += `${uniqId}, https://s3-us-west-1.amazonaws.com/homesnap/bedroom/b${getRandomIntInclusive(1,10)}.jpg, ${homeId}, ${order}, ${randomCaption()}\n`;
     order += 1;
-    uniq_id += 1;
+    uniqId += 1;
   }
 
-  //handle kitchen
-  str += `${uniq_id}, https://s3-us-west-1.amazonaws.com/homesnap/kitchen/k${getRandomIntInclusive(1,10)}.jpg, ${home_id}, ${order}, ${randomCaption()}\n`;
+  // handle kitchen
+  str += `${uniqId}, https://s3-us-west-1.amazonaws.com/homesnap/kitchen/k${getRandomIntInclusive(1,10)}.jpg, ${homeId}, ${order}, ${randomCaption()}\n`;
   order += 1;
-  uniq_id += 1;
+  uniqId += 1;
 
-  //handle diningroom
-  let numDR = getRandomIntInclusive(1,2);
-  for (let i = 0; i < numDR; i++) {
-    str += `${uniq_id}, https://s3-us-west-1.amazonaws.com/homesnap/dining/d${getRandomIntInclusive(1,10)}.jpg, ${home_id}, ${order}, ${randomCaption()}\n`;
+  // handle diningroom
+  const numDR = getRandomIntInclusive(1, 2);
+  for (let i = 0; i < numDR; i += 1) {
+    str += `${uniqId}, https://s3-us-west-1.amazonaws.com/homesnap/dining/d${getRandomIntInclusive(1,10)}.jpg, ${homeId}, ${order}, ${randomCaption()}\n`;
     order += 1;
-    uniq_id += 1;
+    uniqId += 1;
   }
 
-  //handle outer
-  str += `${uniq_id}, https://s3-us-west-1.amazonaws.com/homesnap/outer/o${getRandomIntInclusive(1,10)}.jpg, ${home_id}, ${order}, ${randomCaption()}\n`;
-  uniq_id += 1;
+  // handle outer
+  str += `${uniqId}, https://s3-us-west-1.amazonaws.com/homesnap/outer/o${getRandomIntInclusive(1,10)}.jpg, ${homeId}, ${order}, ${randomCaption()}\n`;
+  uniqId += 1;
 }
 
-//write CSV file
+// write CSV file
 fs.writeFile('database/data.csv', str, (err) => {
   if (err) {
     console.log('Error writing to file');
     return;
   }
   console.log('Data saved');
-})
+});
