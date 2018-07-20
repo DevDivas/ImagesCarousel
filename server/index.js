256bbebf6b1f46a507e8ce70630f8959008b617f
Brian So <soksbrian@gmail.com>
2018-07-20 15:44:01 -0700

    Setup basic express server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-// logger
+// Logger
 app.use((req, res, next) => {
   console.log(req.method, req.url);
+  next();
 });
 
-// serve static assets
+// Serve static assets
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Set port to listen to
+const port = 3001;
+app.listen(port, () => console.log(`Listening to port ${port}...`));
