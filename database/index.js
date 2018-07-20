249a53c5293dec781f71c8f8d0ca711c9ab3dfeb
Brian So <soksbrian@gmail.com>
2018-07-19 20:56:32 -0700

    Insert CSV data into Mongo database

--- database/index.js
+++ database/index.js
@@ -1,7 +1,18 @@
 const fs = require('fs');
-// const csv = require('csv-parser');
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/assets');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/assets');
+
+// define schema
+let assetSchema = mongoose.Schema({
+  id: {type: Number, unique: true},
+  url: String,
+  home_id: Number,
+  order: Number,
+  caption: String
+});
+
+// defining model
+let Asset = mongoose.model('Asset', assetSchema);
 
 fs.readFile(__dirname+'/data.csv', 'utf8', (err, data) => {
   if (err) {
@@ -9,19 +20,32 @@ fs.readFile(__dirname+'/data.csv', 'utf8', (err, data) => {
     return;
   }
   console.log('Data fetched from CSV file');
-  console.log(data.split('\n'));
+  let strAssets = data.split('\n');
+
+  // refactor string entries to object entries
+  let objAssets = strAssets.map((str) => {
+    let temp = str.split(', ');
+    return {
+      id: temp[0],
+      url: temp[1],
+      home_id: temp[2],
+      order: temp[3],
+      caption: temp[4]
+    }
+  })
+  
+  // insert documents into collection
+  Asset.insertMany(objAssets, (err, docs) => {
+    if (err) {
+      console.log('InsertMany error: ', err);
+      return;
+    }
+    console.log('InsertMany success');
+  })
 })
 
 
-// let assetSchema = mongoose.Schema({
-//   id: {type: Number, unique: true},
-//   url: String,
-//   home_id: Number,
-//   order: Number,
-//   caption: String
-// });
 
-// let Asset = mongoose.model('Asset', assetSchema);
 
 
 
