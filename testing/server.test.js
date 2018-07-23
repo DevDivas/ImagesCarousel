f3a83027ef0473f9d10a9668502130de0a0e85ed
Brian So <soksbrian@gmail.com>
2018-07-23 15:57:21 -0500

    Write preliminary tests for server

--- testing/server.test.js
+++ testing/server.test.js
@@ -11,21 +11,34 @@ describe('Test the root path', () => {
   });
 });
 
-// Check to see if response exisits for visiting room page 
-describe('Test response data type', () => {
-  test('should respond with json objects', () => {
-    return request(app).get('/room/12').then((response) => {
-      expect(response).toBeDefined();
-    });
+describe('Test fetching room pics', () => {
+  test('should have the same unique exterior object', () => {
+    db.fetchRoomPics(12, (err, docs) => {
+      if (err) {
+        console.log('error during testing');
+        return;
+      }
+      const expected = {"id":69,"url":"https://s3-us-west-1.amazonaws.com/homesnap/exterior/h12.jpg","home_id":12,"order":1,"caption":"''"};
+      expect(JSON.stringify(docs[0])).toEqual(JSON.stringify(expected));
+    })
   });
 });
+
+// Check to see if response exisits for visiting room page 
+// describe('Test response data type', () => {
+//   test('response should exist', () => {
+//     return request(app).get('/rooms/12').then((response) => {
+//       expect(response.data).toBeUndefined();
+//     });
+//   });
+// });
 
 // Check to see if the correct room is served when specified
 // describe('Test response unique room', () => {
 //   test('should respond with unique room object', () => {
 //     const expected = {"id":69,"url":"https://s3-us-west-1.amazonaws.com/homesnap/exterior/h12.jpg","home_id":12,"order":1,"caption":"''"};
-//     return request(app).get('/room/12').then((response) => {
-//       expect(response).toContain(expected);
+//     return request(app).get('/rooms/12').then((response) => {
+//       expect(response.data[0]).toEqual(expected);
 //     });
 //   });
 // });
