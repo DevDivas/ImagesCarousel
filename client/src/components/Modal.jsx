4107b4ad050633ae891966abf65658cb286406af
Brian So <soksbrian@gmail.com>
2018-07-24 14:44:08 -0500

    Format modal close button

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -6,8 +6,10 @@ const React = require('react');
 const Modal = (props) => {
   return (
     <div className="modal">
-      <div>THIS IS THE MODAL</div>
-      <Carousel collection={props.collection} />
+      <span className="closeBtn">&times;</span>
+      <div className="modal-content">THIS IS THE MODAL
+        <Carousel collection={props.collection} />
+      </div>
     </div>
   );
 };
