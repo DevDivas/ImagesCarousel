33ed38f4e02a3e251e30a7523fb90febf4b26344
Brian So <soksbrian@gmail.com>
2018-07-24 10:11:36 -0500

    Refactor server test code

--- testing/server.test.js
+++ testing/server.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('../server/index.js');
 const db = require('../database/index.js');
 
-// Check to see if a GET request is sent upon visiting root path
-describe('Test the root path', () => {
-  test('should response a GET method', () => {
-    return request(app).get('/').then((response) => { 
+// Check to see if a GET request is sent upon visiting room page
+describe('Test the room path', () => {
+  test('should response to GET method', () => {
+    return request(app).get('/rooms/12').then((response) => { 
       expect(response.statusCode).toBe(200);
     });
   });
@@ -20,18 +20,9 @@ describe('Test fetching room pics', () => {
       }
       const expected = {"id":69,"url":"https://s3-us-west-1.amazonaws.com/homesnap/exterior/h12.jpg","home_id":12,"order":1,"caption":"''"};
       expect(JSON.stringify(docs[0])).toEqual(JSON.stringify(expected));
-    })
+    });
   });
 });
-
-// Check to see if response exisits for visiting room page 
-// describe('Test response data type', () => {
-//   test('response should exist', () => {
-//     return request(app).get('/rooms/12').then((response) => {
-//       expect(response.data).toBeUndefined();
-//     });
-//   });
-// });
 
 // Check to see if the correct room is served when specified
 // describe('Test response unique room', () => {
