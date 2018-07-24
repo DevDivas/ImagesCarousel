255a6eec3115d4642a5081bf0c2240cc0c6e1c8b
Brian So <soksbrian@gmail.com>
2018-07-24 17:21:17 -0500

    Add responsive arrow buttons

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -7,6 +7,8 @@ const Modal = (props) => {
   return (
     <div className="modal">
       <span className="closeBtn">&times;</span>
+      <span className="prevBtn">&#8249;</span>
+      <span className="nextBtn">&#8250;</span>
       <div className="modal-content">
         <span>
           <img src={props.staged.url} className="focusImage" alt="" />
