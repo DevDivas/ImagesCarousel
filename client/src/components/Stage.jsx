7451771b0261ffdea8d83b36322a47dc56327e74
Brian So <soksbrian@gmail.com>
2018-07-27 16:13:35 -0500

    Add static view, share, save btns

--- client/src/components/Stage.jsx
+++ client/src/components/Stage.jsx
@@ -4,11 +4,11 @@ require('../css/stage.css');
 const React = require('react');
 
 const Stage = ({ staged, handleStageClick }) => (
-  <div>
-    <div className="stage" onClick={handleStageClick} onKeyPress={() => ''} role="presentation">
-      <img src={staged.url} className="stage-img" alt="" />
-    </div>
+  <div className="stage" onClick={handleStageClick} onKeyPress={() => ''} role="presentation">
+    <img src={staged.url} className="stage-img" alt="" />
     <input type="submit" className="viewPhotosBtn" value="View Photos" />
+    <input type="submit" className="shareBtn" value="Share" />
+    <input type="submit" className="saveBtn" value="Save" />
   </div>
 );
 
