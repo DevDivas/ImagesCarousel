e4f91fb05ed433698f89f16b44d4d61ac8ca2a49
Brian So <soksbrian@gmail.com>
2018-07-30 16:48:45 -0700

    Refactor Carousel to class

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,50 +4,67 @@ require('../css/carousel.css');
 require('../css/thumbnail.css');
 const React = require('react');
 
-const Carousel = ({ appState, handleThumbnailClick, handleCarouselToggle }) => (
-  <div className="carouselFrame">
-    <div className={appState.showCarousel ? 'infoPanel-top' : 'infoPanel-bottom'} onMouseEnter={appState.showCarousel ? '' : handleCarouselToggle}>
-      <span className="imgInfo">
-        {appState.focusImage.order}
-        /
-        {appState.collection.length}
-        :
-        {appState.focusImage.caption}
-      </span>
-      <div>
-        <span className="carousel-toggle" onClick={handleCarouselToggle} onKeyPress={() => ''} role="presentation">
-          {
-            appState.showCarousel ? 'Hide photo list' : 'Show photo list'
-          }
-        </span>
-      </div>
-    </div>
-    <div className="carousel-wrapper">
-      {
-        appState.showCarousel
-          ? (
-            <ul className="carousel">
+class Carousel extends React.Component {
+  componentDidUpdate(prevProps) {
+    const { appState } = this.props;
+    const { focusImage, collection } = appState;
+    const { order } = focusImage;
+    console.log(order);
+
+    // if (collection.length > 7) {
+      
+    // }
+  }
+
+  render() {
+    const { appState, handleThumbnailClick, handleCarouselToggle } = this.props;
+
+    return (
+      <div className="carouselFrame">
+        <div className={appState.showCarousel ? 'infoPanel-top' : 'infoPanel-bottom'} onMouseEnter={appState.showCarousel ? () => {} : handleCarouselToggle}>
+          <span className="imgInfo">
+            {appState.focusImage.order}
+            /
+            {appState.collection.length}
+            :
+            {appState.focusImage.caption}
+          </span>
+          <div>
+            <span className="carousel-toggle" onClick={handleCarouselToggle} onKeyPress={() => ''} role="presentation">
               {
-                appState.collection.map((pic) => {
-                  const id = Number(pic.id);
-                  return (
-                    <span key={id} onClick={e => handleThumbnailClick(e)} onKeyPress={() => ''} role="presentation">
-                      {
-                        pic.url === appState.focusImage.url
-                          ? <img src={pic.url} className="thumbnail-selected" alt="" />
-                          : <img src={pic.url} className="thumbnail" alt="" />
-                      }
-                    </span>
-                  );
-                })
+                appState.showCarousel ? 'Hide photo list' : 'Show photo list'
               }
-            </ul>
-          )
-          : <span />
-      }
-    </div>
-  </div>
-);
+            </span>
+          </div>
+        </div>
+        <div className="carousel-wrapper">
+          {
+            appState.showCarousel
+              ? (
+                <ul className="carousel" style={{ transform: 'translate(-210px, 0px)' }}>
+                  {
+                    appState.collection.map((pic) => {
+                      const id = Number(pic.id);
+                      return (
+                        <span key={id} onClick={e => handleThumbnailClick(e)} onKeyPress={() => ''} role="presentation">
+                          {
+                            pic.url === appState.focusImage.url
+                              ? <img src={pic.url} className="thumbnail-selected" alt="" />
+                              : <img src={pic.url} className="thumbnail" alt="" />
+                          }
+                        </span>
+                      );
+                    })
+                  }
+                </ul>
+              )
+              : <span />
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 Carousel.propTypes = {
   appState: PropTypes.object.isRequired,
